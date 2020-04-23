acd16e513f5f7ab480629f5dae810b0ad7cd0b51
Almnir <almnir.mail@gmail.com>
2020-04-23 15:00:54 +0300

    first commit

---
+++ SoundLibrariesTest/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace SoundLibrariesTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonStartRecord = new System.Windows.Forms.Button();
+            this.buttonStopRecord = new System.Windows.Forms.Button();
+            this.buttonStartPlay = new System.Windows.Forms.Button();
+            this.buttonStopPlay = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonStartRecord
+            // 
+            this.buttonStartRecord.Location = new System.Drawing.Point(170, 86);
+            this.buttonStartRecord.Name = "buttonStartRecord";
+            this.buttonStartRecord.Size = new System.Drawing.Size(75, 23);
+            this.buttonStartRecord.TabIndex = 0;
+            this.buttonStartRecord.Text = "Start";
+            this.buttonStartRecord.UseVisualStyleBackColor = true;
+            this.buttonStartRecord.Click += new System.EventHandler(this.buttonStartRecord_Click);
+            // 
+            // buttonStopRecord
+            // 
+            this.buttonStopRecord.Location = new System.Drawing.Point(170, 137);
+            this.buttonStopRecord.Name = "buttonStopRecord";
+            this.buttonStopRecord.Size = new System.Drawing.Size(75, 23);
+            this.buttonStopRecord.TabIndex = 1;
+            this.buttonStopRecord.Text = "StopRecord";
+            this.buttonStopRecord.UseVisualStyleBackColor = true;
+            this.buttonStopRecord.Click += new System.EventHandler(this.buttonStopRecord_Click);
+            // 
+            // buttonStartPlay
+            // 
+            this.buttonStartPlay.Location = new System.Drawing.Point(321, 86);
+            this.buttonStartPlay.Name = "buttonStartPlay";
+            this.buttonStartPlay.Size = new System.Drawing.Size(75, 23);
+            this.buttonStartPlay.TabIndex = 2;
+            this.buttonStartPlay.Text = "Play";
+            this.buttonStartPlay.UseVisualStyleBackColor = true;
+            this.buttonStartPlay.Click += new System.EventHandler(this.buttonStartPlay_Click);
+            // 
+            // buttonStopPlay
+            // 
+            this.buttonStopPlay.Location = new System.Drawing.Point(321, 136);
+            this.buttonStopPlay.Name = "buttonStopPlay";
+            this.buttonStopPlay.Size = new System.Drawing.Size(75, 23);
+            this.buttonStopPlay.TabIndex = 3;
+            this.buttonStopPlay.Text = "StopPlay";
+            this.buttonStopPlay.UseVisualStyleBackColor = true;
+            this.buttonStopPlay.Click += new System.EventHandler(this.buttonStopPlay_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonStopPlay);
+            this.Controls.Add(this.buttonStartPlay);
+            this.Controls.Add(this.buttonStopRecord);
+            this.Controls.Add(this.buttonStartRecord);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStartRecord;
+        private System.Windows.Forms.Button buttonStopRecord;
+        private System.Windows.Forms.Button buttonStartPlay;
+        private System.Windows.Forms.Button buttonStopPlay;
+    }
+}
+
---
+++ SoundLibrariesTest/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SoundLibrariesTest
+{
+    public partial class Form1 : Form
+    {
+        string sDestinationFile;
+
+        public Form1()
+        {
+            InitializeComponent();
+            sDestinationFile = "";
+        }
+
+        //MediaPlayer mediaPlayer = new MediaPlayer();
+
+        //private void PlayByMediaPlayer()
+        //{
+        //    Uri myUri = new Uri(@"e:\1\file_example_OOG_1MG.ogg", UriKind.Absolute);
+        //    mediaPlayer.Open(myUri);
+        //    mediaPlayer.Play();
+        //}
+
+        //private void StopByMediaPlayer()
+        //{
+        //    mediaPlayer?.Stop();
+        //}
+
+        [DllImport("winmm.dll")]
+        private static extern long mciSendString(
+            string command,
+            StringBuilder returnValue,
+            int returnLength,
+            IntPtr winHandle);
+
+        private void PlayByWINNM()
+        {
+            if (string.IsNullOrEmpty(sDestinationFile)) return;
+
+            StringBuilder err = new StringBuilder();
+            mciSendString("open " + sDestinationFile + " type waveaudio alias thisIsMyTag", err, 0, IntPtr.Zero);
+            //mciSendString("open " + sDestinationFile + " type mpegvideo alias thisIsMyTag", err, 0, IntPtr.Zero);
+            mciSendString("play thisIsMyTag", err, 0, IntPtr.Zero);
+            if (!string.IsNullOrEmpty(err.ToString()))
+            {
+                MessageBox.Show(err.ToString());
+            }
+        }
+
+        private void RecordByWINNM()
+        {
+            StringBuilder err = new StringBuilder();
+            mciSendString("open new type waveaudio alias mywav", err, 0, IntPtr.Zero);
+            mciSendString("set mywav bitspersample 8 samplespersec 8000 channels 1", err, 0, IntPtr.Zero);
+            mciSendString("record mywav", err, 0, IntPtr.Zero);
+            if (!string.IsNullOrEmpty(err.ToString()))
+            {
+                MessageBox.Show(err.ToString());
+            }
+
+        }
+        private void StopRecordByWINNM()
+        {
+            string rndm = new Random().Next().ToString() + "file.";
+            //Get the default music directory of Windows:
+            sDestinationFile = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic) + "\\" + rndm + "wav";
+
+            StringBuilder err = new StringBuilder();
+            mciSendString("stop mywav", err, 0, IntPtr.Zero);
+            mciSendString("save mywav " + sDestinationFile, err, 0, IntPtr.Zero);
+            mciSendString("close mywav", err, 0, IntPtr.Zero);
+            if (!string.IsNullOrEmpty(err.ToString()))
+            {
+                MessageBox.Show(err.ToString());
+            }
+
+        }
+
+        private void StopByWINNM()
+        {
+            mciSendString("stop thisIsMyTag", null, 0, IntPtr.Zero);
+            mciSendString("close thisIsMyTag", null, 0, IntPtr.Zero);
+        }
+
+        private void buttonStartRecord_Click(object sender, EventArgs e)
+        {
+            RecordByWINNM();
+        }
+
+        private void buttonStopRecord_Click(object sender, EventArgs e)
+        {
+            StopRecordByWINNM();
+        }
+
+        private void buttonStartPlay_Click(object sender, EventArgs e)
+        {
+            PlayByWINNM();
+        }
+
+        private void buttonStopPlay_Click(object sender, EventArgs e)
+        {
+            StopByWINNM();
+        }
+    }
+}
