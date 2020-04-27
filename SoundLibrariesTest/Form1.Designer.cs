6936a59b796385f470cd81eea3632ed9899afa32
Almnir <almnir.mail@gmail.com>
2020-04-27 21:52:29 +0300

    Теперь записывает и проигрывает через CSCore.

--- SoundLibrariesTest/Form1.Designer.cs
+++ SoundLibrariesTest/Form1.Designer.cs
@@ -32,6 +32,8 @@
             this.buttonStopRecord = new System.Windows.Forms.Button();
             this.buttonStartPlay = new System.Windows.Forms.Button();
             this.buttonStopPlay = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.listView1 = new System.Windows.Forms.ListView();
             this.SuspendLayout();
             // 
             // buttonStartRecord
@@ -74,17 +76,39 @@
             this.buttonStopPlay.UseVisualStyleBackColor = true;
             this.buttonStopPlay.Click += new System.EventHandler(this.buttonStopPlay_Click);
             // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(170, 258);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(553, 21);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(170, 328);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(553, 97);
+            this.listView1.TabIndex = 5;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.buttonStopPlay);
             this.Controls.Add(this.buttonStartPlay);
             this.Controls.Add(this.buttonStopRecord);
             this.Controls.Add(this.buttonStartRecord);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
@@ -95,6 +119,8 @@
         private System.Windows.Forms.Button buttonStopRecord;
         private System.Windows.Forms.Button buttonStartPlay;
         private System.Windows.Forms.Button buttonStopPlay;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ListView listView1;
     }
 }
 
--- SoundLibrariesTest/Form1.cs
+++ SoundLibrariesTest/Form1.cs
@@ -1,4 +1,14 @@
-﻿using System;
+﻿using CSCore;
+using CSCore.Codecs;
+using CSCore.Codecs.WAV;
+using CSCore.CoreAudioAPI;
+using CSCore.SoundIn;
+using CSCore.SoundOut;
+using CSCore.Streams;
+using CSCore.Win32;
+using System;
+using System.Collections.ObjectModel;
+using System.Globalization;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Windows.Forms;
@@ -8,6 +18,11 @@ namespace SoundLibrariesTest
     public partial class Form1 : Form
     {
         string sDestinationFile;
+        private IWriteable _writer;
+        private ISoundOut _soundOut;
+        private WasapiCapture _soundIn;
+        private IWaveSource _finalSource;
+        private readonly ObservableCollection<MMDevice> _devices = new ObservableCollection<MMDevice>();
 
         public Form1()
         {
@@ -64,9 +79,7 @@ namespace SoundLibrariesTest
         }
         private void StopRecordByWINNM()
         {
-            string rndm = new Random().Next().ToString() + "file.";
-            //Get the default music directory of Windows:
-            sDestinationFile = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic) + "\\" + rndm + "wav";
+            GetFilePath();
 
             StringBuilder err = new StringBuilder();
             mciSendString("stop mywav", err, 0, IntPtr.Zero);
@@ -79,30 +92,142 @@ namespace SoundLibrariesTest
 
         }
 
+        private void GetFilePath()
+        {
+            string rndm = new Random().Next().ToString() + "file.";
+            //Get the default music directory of Windows:
+            sDestinationFile = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic) + "\\" + rndm + "wav";
+        }
+
         private void StopByWINNM()
         {
             mciSendString("stop thisIsMyTag", null, 0, IntPtr.Zero);
             mciSendString("close thisIsMyTag", null, 0, IntPtr.Zero);
         }
 
+        void StartRecordByCSCore()
+        {
+            GetFilePath();
+            _soundIn = new WasapiCapture();
+            _soundIn.Device = (MMDevice)listView1.SelectedItems[0].Tag;
+            _soundIn.Initialize();
+            var soundInSource = new SoundInSource(_soundIn);
+            var singleBlockNotificationStream = new SingleBlockNotificationStream(soundInSource.ToSampleSource());
+            _finalSource = singleBlockNotificationStream.ToWaveSource();
+            _writer = new WaveWriter(sDestinationFile, _finalSource.WaveFormat);
+            byte[] buffer = new byte[_finalSource.WaveFormat.BytesPerSecond / 2];
+            soundInSource.DataAvailable += (s, e) =>
+            {
+                int read;
+                while ((read = _finalSource.Read(buffer, 0, buffer.Length)) > 0)
+                    _writer.Write(buffer, 0, read);
+            };
+
+            _soundIn.Start();
+
+        }
+
+        void StopRecordByCSCore()
+        {
+            if (_soundIn != null)
+            {
+                _soundIn.Stop();
+                _soundIn.Dispose();
+                _soundIn = null;
+                _finalSource.Dispose();
+
+                if (_writer is IDisposable)
+                    ((IDisposable)_writer).Dispose();
+            }
+        }
+
+        void StartPlayByCSCore()
+        {
+            IWaveSource _waveSource = CodecFactory.Instance.GetCodec(sDestinationFile)
+                .ToSampleSource()
+                .ToMono()
+                .ToWaveSource();
+            _soundOut = new WasapiOut() { Latency = 100, Device = (MMDevice)comboBox1.SelectedItem };
+            _soundOut.Initialize(_waveSource);
+            _soundOut.Play();
+        }
+
+        void StopPlayByCSCore()
+        {
+            if (_soundOut != null)
+            {
+                _soundOut.Stop();
+                _soundOut.Dispose();
+                _soundOut = null;
+            }
+                
+        }
+
         private void buttonStartRecord_Click(object sender, EventArgs e)
         {
-            RecordByWINNM();
+            StartRecordByCSCore();
         }
 
         private void buttonStopRecord_Click(object sender, EventArgs e)
         {
-            StopRecordByWINNM();
+            StopRecordByCSCore();
         }
 
         private void buttonStartPlay_Click(object sender, EventArgs e)
         {
-            PlayByWINNM();
+            StartPlayByCSCore();
         }
 
         private void buttonStopPlay_Click(object sender, EventArgs e)
         {
-            StopByWINNM();
+            StopPlayByCSCore();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            using (var mmdeviceEnumerator = new MMDeviceEnumerator())
+            {
+                using (
+                    var mmdeviceCollection = mmdeviceEnumerator.EnumAudioEndpoints(DataFlow.Render, DeviceState.Active))
+                {
+                    foreach (var device in mmdeviceCollection)
+                    {
+                        _devices.Add(device);
+                    }
+                }
+            }
+            RefreshDevices();
+            comboBox1.DataSource = _devices;
+            comboBox1.DisplayMember = "FriendlyName";
+            comboBox1.ValueMember = "DeviceID";
+        }
+
+        private void RefreshDevices()
+        {
+            listView1.Items.Clear();
+
+            using (var deviceEnumerator = new MMDeviceEnumerator())
+            using (var deviceCollection = deviceEnumerator.EnumAudioEndpoints(
+                DataFlow.Capture, DeviceState.Active))
+            {
+                foreach (var device in deviceCollection)
+                {
+                    var deviceFormat = WaveFormatFromBlob(device.PropertyStore[
+                        new PropertyKey(new Guid(0xf19f064d, 0x82c, 0x4e27, 0xbc, 0x73, 0x68, 0x82, 0xa1, 0xbb, 0x8e, 0x4c), 0)].BlobValue);
+
+                    var item = new ListViewItem(device.FriendlyName) { Tag = device };
+                    item.SubItems.Add(deviceFormat.Channels.ToString(CultureInfo.InvariantCulture));
+
+                    listView1.Items.Add(item);
+                }
+            }
+        }
+        private static WaveFormat WaveFormatFromBlob(Blob blob)
+        {
+            if (blob.Length == 40)
+                return (WaveFormat)Marshal.PtrToStructure(blob.Data, typeof(WaveFormatExtensible));
+            return (WaveFormat)Marshal.PtrToStructure(blob.Data, typeof(WaveFormat));
+        }
+
     }
 }
